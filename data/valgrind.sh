f3c0daeca364bac2d967793517d737aea37f8d0c
Knoblauchpilze <prolific-programmer@tuta.com>
2019-12-20 23:43:14 +0100

    Added script to handle valgrind

--- data/valgrind.sh
+++ data/valgrind.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 CURR_DIR=$(dirname $0)
-valgrind --leak-check=yes --leak-check=full --show-leak-kinds=all ./bin/cellular_automaton $(cat data/config/local)
+valgrind --log-file="analysis" --leak-check=yes --leak-check=full --show-leak-kinds=all ./bin/cellular_automaton $(cat data/config/local)
