207555c15abdfa179c37f6b221634d80528f8883
Knoblauchpilze <prolific-programmer@tuta.com>
2019-12-30 20:19:38 +0100

    Last push before switching back to big ol PC

--- src/CellsQuadTreeNode.cc
+++ src/CellsQuadTreeNode.cc
@@ -282,10 +282,10 @@ namespace cellulator {
     // corresponds to all the cells except the boundaries, where we would need to
     // access the data from adjacent nodes. In the meantime we need to update the
     // adjacency count.
-    for (int y = 0 ; y < m_area.h() ; ++y) {
+    for (int y = 1 ; y < m_area.h() - 1 ; ++y) {
       int offset = y * m_area.w();
 
-      for (int x = 0 ; x < m_area.w() ; ++x) {
+      for (int x = 1 ; x < m_area.w() - 1 ; ++x) {
         State s = m_cells[offset + x].update(m_adjacency[offset + x]);
         bool alive = (s == State::Alive || s == State::Newborn);
 
