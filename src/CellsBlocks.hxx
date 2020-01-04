54fa565146baee7ee373a939cb824f4b3d1eaa4c
Knoblauchpilze <prolific-programmer@tuta.com>
2020-01-04 17:51:28 +0100

    Started working on implementation for 'CellsBlocks'

--- src/CellsBlocks.hxx
+++ src/CellsBlocks.hxx
@@ -5,6 +5,25 @@
 
 namespace cellulator {
 
+  inline
+  float
+  CellsBlocks::getDeadCellProbability() noexcept {
+    return 0.7f;
+  }
+
+  inline
+  unsigned
+  CellsBlocks::dataIDFromBlock(unsigned blockID) const noexcept {
+    return blockID * sizeOfBlock();
+  }
+
+  inline
+  unsigned
+  CellsBlocks::sizeOfBlock() const noexcept {
+    return m_nodesDims.area();
+  }
+
+
 }
 
 #endif    /* CELLS_BLOCKS_HXX */
